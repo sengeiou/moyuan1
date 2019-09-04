323c2ab598985f311db7af76ee1d3c7fdf2ef3e2
j309999174 <309999174@qq.com>
2019-09-05 03:45:28 +0800

    会话设置页面

--- app/src/main/java/xin/banghua/beiyuan/ConversationActivity.java
+++ app/src/main/java/xin/banghua/beiyuan/ConversationActivity.java
@@ -1,25 +1,85 @@
 package xin.banghua.beiyuan;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import io.rong.imkit.RongIM;
+import io.rong.imlib.RongIMClient;
+import io.rong.imlib.model.Conversation;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import xin.banghua.beiyuan.Main3Branch.ConversationSettingActivity;
+import xin.banghua.beiyuan.ParseJSON.ParseJSONObject;
+import xin.banghua.beiyuan.Personage.PersonageActivity;
+import xin.banghua.beiyuan.SharedPreferences.SharedHelper;
+
+import static io.rong.imlib.model.Conversation.ConversationNotificationStatus.DO_NOT_DISTURB;
+import static io.rong.imlib.model.Conversation.ConversationNotificationStatus.NOTIFY;
 
 
 public class ConversationActivity extends AppCompatActivity {
 
+    private static final String TAG = "ConversationActivity";
+
+    CircleImageView portrait;
+    TextView nickname;
+    Switch istop,donotdisturb;
+    Button recored_clear,blacklist_btn;
+
+    String title;
+    String targetId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
 
+        Intent intent = getIntent();
+        title = intent.getData().getQueryParameter("title") ;
+        Log.d(TAG, "onCreate: 人名："+title);
+        targetId = intent.getData().getQueryParameter("targetId") ;
+        Log.d(TAG, "onCreate: id："+targetId);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(title);
+
 
     }
 
+    @Override  //菜单的填充
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_toolbar_conversation, menu);
+        return true;
+    }
     @Override  //菜单的点击，其中返回键的id是android.R.id.home
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -28,9 +88,13 @@ public class ConversationActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
+        if (id == R.id.action_conversation_personpage) {
+            Intent intent = new Intent(ConversationActivity.this, ConversationSettingActivity.class);
+            intent.putExtra("targetId",targetId);
+            intent.putExtra("title",title);
+            startActivity(intent);
+            return true;
+        }
         switch (item.getItemId()) {
             case android.R.id.home:
                 this.finish(); // back button
---
+++ app/src/main/java/xin/banghua/beiyuan/Main3Branch/ConversationSettingActivity.java
@@ -0,0 +1,301 @@
+package xin.banghua.beiyuan.Main3Branch;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import io.rong.imkit.RongIM;
+import io.rong.imlib.RongIMClient;
+import io.rong.imlib.model.Conversation;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import xin.banghua.beiyuan.ParseJSON.ParseJSONObject;
+import xin.banghua.beiyuan.Personage.PersonageActivity;
+import xin.banghua.beiyuan.R;
+import xin.banghua.beiyuan.SharedPreferences.SharedHelper;
+
+import static io.rong.imlib.model.Conversation.ConversationNotificationStatus.DO_NOT_DISTURB;
+import static io.rong.imlib.model.Conversation.ConversationNotificationStatus.NOTIFY;
+
+public class ConversationSettingActivity extends AppCompatActivity {
+    private static final String TAG = "ConversationSettingActi";
+
+
+    CircleImageView portrait;
+    TextView nickname;
+    Switch istop,donotdisturb;
+    Button recored_clear,blacklist_btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation_setting);
+
+        Intent intent = getIntent();
+        final String targetId = intent.getStringExtra("targetId");
+        final String title = intent.getStringExtra("title");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(title);
+
+
+        portrait = findViewById(R.id.portrait);
+        portrait.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), PersonageActivity.class);
+                intent.putExtra("userID",targetId);
+                Log.d(TAG, "onClick: 跳转个人页面");
+                v.getContext().startActivity(intent);
+            }
+        });
+        nickname = findViewById(R.id.nickname);
+        nickname.setText(title);
+        RongIM.getInstance().getConversation(Conversation.ConversationType.PRIVATE, targetId, new RongIMClient.ResultCallback<Conversation>() {
+            @Override
+            public void onSuccess(Conversation conversation) {
+                Log.d(TAG, "onSuccess: 获取会话成功");
+                if (conversation.isTop()){
+                    istop.setChecked(true);
+                }else {
+                    istop.setChecked(false);
+                }
+                if (conversation.getNotificationStatus()==DO_NOT_DISTURB){
+                    donotdisturb.setChecked(true);
+                }else {
+                    donotdisturb.setChecked(false);
+                }
+            }
+
+            @Override
+            public void onError(RongIMClient.ErrorCode errorCode) {
+                Log.d(TAG, "onSuccess: 获取会话失败");
+            }
+        });
+        istop = findViewById(R.id.istop);
+        istop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    RongIM.getInstance().setConversationToTop(Conversation.ConversationType.PRIVATE, targetId, true, new RongIMClient.ResultCallback<Boolean>() {
+                        @Override
+                        public void onSuccess(Boolean aBoolean) {
+                            Toast.makeText(getApplicationContext(),"已开启置顶",Toast.LENGTH_LONG).show();
+                        }
+
+                        @Override
+                        public void onError(RongIMClient.ErrorCode errorCode) {
+
+                        }
+                    });
+                }else {
+                    RongIM.getInstance().setConversationToTop(Conversation.ConversationType.PRIVATE, targetId, false, new RongIMClient.ResultCallback<Boolean>() {
+                        @Override
+                        public void onSuccess(Boolean aBoolean) {
+                            Toast.makeText(getApplicationContext(),"已关闭置顶",Toast.LENGTH_LONG).show();
+                        }
+
+                        @Override
+                        public void onError(RongIMClient.ErrorCode errorCode) {
+
+                        }
+                    });
+                }
+            }
+        });
+        donotdisturb = findViewById(R.id.donot_disturb);
+        donotdisturb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    RongIM.getInstance().setConversationNotificationStatus(Conversation.ConversationType.PRIVATE, targetId, DO_NOT_DISTURB, new RongIMClient.ResultCallback<Conversation.ConversationNotificationStatus>() {
+                        @Override
+                        public void onSuccess(Conversation.ConversationNotificationStatus conversationNotificationStatus) {
+                            Toast.makeText(getApplicationContext(),"已开启免打扰",Toast.LENGTH_LONG).show();
+                        }
+
+                        @Override
+                        public void onError(RongIMClient.ErrorCode errorCode) {
+
+                        }
+                    });
+                }else {
+                    RongIM.getInstance().setConversationNotificationStatus(Conversation.ConversationType.PRIVATE, targetId, NOTIFY, new RongIMClient.ResultCallback<Conversation.ConversationNotificationStatus>() {
+                        @Override
+                        public void onSuccess(Conversation.ConversationNotificationStatus conversationNotificationStatus) {
+                            Toast.makeText(getApplicationContext(),"已关闭免打扰",Toast.LENGTH_LONG).show();
+                        }
+
+                        @Override
+                        public void onError(RongIMClient.ErrorCode errorCode) {
+
+                        }
+                    });
+                }
+            }
+        });
+        recored_clear = findViewById(R.id.record_clear);
+        recored_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RongIM.getInstance().deleteMessages(Conversation.ConversationType.PRIVATE, targetId, new RongIMClient.ResultCallback<Boolean>() {
+                    @Override
+                    public void onSuccess(Boolean aBoolean) {
+                        Toast.makeText(getApplicationContext(),"已清除会话消息",Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(RongIMClient.ErrorCode errorCode) {
+
+                    }
+                });
+            }
+        });
+        blacklist_btn = findViewById(R.id.blacklist_btn);
+        blacklist_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addBlacklist("https://applet.banghua.xin/app/index.php?i=99999&c=entry&a=webapp&do=addblacklist&m=socialchat",targetId);
+            }
+        });
+
+        getDataPersonage("https://applet.banghua.xin/app/index.php?i=99999&c=entry&a=webapp&do=personage&m=socialchat",targetId);
+    }
+    public void initPersonage(JSONObject jsonObject) throws JSONException {
+        Glide.with(this)
+                .asBitmap()
+                .load(jsonObject.getString("portrait"))
+                .into(portrait);
+    }
+    //网络数据部分
+    @SuppressLint("HandlerLeak")
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            //1是用户数据，2是幻灯片
+            switch (msg.what){
+                case 1:
+                    try {
+                        String resultJson1 = msg.obj.toString();
+                        Log.d(TAG, "handleMessage: 用户数据接收的值"+msg.obj.toString());
+
+                        JSONObject jsonObject = new ParseJSONObject(msg.obj.toString()).getParseJSON();
+                        initPersonage(jsonObject);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case 3:
+                    Toast.makeText(getApplicationContext(),"已加入黑名单",Toast.LENGTH_LONG).show();
+                    break;
+
+            }
+        }
+    };
+
+    //TODO okhttp获取用户信息
+    public void getDataPersonage(final String url,final String targetId){
+        new Thread(new Runnable() {
+            @Override
+            public void run(){
+                OkHttpClient client = new OkHttpClient();
+                RequestBody formBody = new FormBody.Builder()
+                        .add("userid", targetId)
+                        .build();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(formBody)
+                        .build();
+
+                try (Response response = client.newCall(request).execute()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    Message message=handler.obtainMessage();
+                    message.obj=response.body().string();
+                    message.what=1;
+                    Log.d(TAG, "run: getDataPersonage"+message.obj.toString());
+                    handler.sendMessageDelayed(message,10);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    //TODO okhttp加入黑名单
+    public void addBlacklist(final String url,final String targetId){
+        //Toast.makeText(mContext, "已加入黑名单", Toast.LENGTH_LONG).show();
+        new Thread(new Runnable() {
+            @Override
+            public void run(){
+                SharedHelper shuserinfo = new SharedHelper(getApplicationContext());
+                String myid = shuserinfo.readUserInfo().get("userID");
+
+                OkHttpClient client = new OkHttpClient();
+                RequestBody formBody = new FormBody.Builder()
+                        .add("myid", myid)
+                        .add("yourid", targetId)
+                        .build();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(formBody)
+                        .build();
+
+                try (Response response = client.newCall(request).execute()) {
+                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+                    Message message=handler.obtainMessage();
+                    message.obj=response.body().string();
+                    message.what=3;
+                    Log.d(TAG, "run: Userinfo发送的值"+message.obj.toString());
+                    handler.sendMessageDelayed(message,10);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    @Override  //菜单的点击，其中返回键的id是android.R.id.home
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish(); // back button
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
